72fa40d7d0eb6aa0e412fd471bc85d0f638b1922
Ruslan <gadjat@mail.ru>
2022-09-20 22:03:54 +0300

    2 - add isOk method and abstract repo

--- src/main/java/com/javarush/quest/bulimov/questdelta/repository/AbstractRepository.java
+++ src/main/java/com/javarush/quest/bulimov/questdelta/repository/AbstractRepository.java
@@ -1,4 +1,51 @@
 package com.javarush.quest.bulimov.questdelta.repository;
 
-public abstract class AbstractRepository implements Repository{
+import com.javarush.quest.bulimov.questdelta.entity.AbstractEntity;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Function;
+
+public abstract class AbstractRepository<T extends AbstractEntity> implements Repository<T>{
+
+    public static final AtomicLong id = new AtomicLong(System.currentTimeMillis());
+    protected final Map<Long, T> map = new HashMap<>();
+
+    @Override
+    public Collection<T> getAll(){
+        return map.values();
+    }
+
+    @Override
+    public abstract Collection<T> find(T pattern);
+
+    protected <V> boolean isOk(T pattern, T current, Function<T, V> fieldGetter) {
+        V currentFieldValue = fieldGetter.apply(current);
+        V patternFieldValue = fieldGetter.apply(pattern);
+        return Objects.isNull(patternFieldValue) || patternFieldValue.equals(currentFieldValue);
+    }
+
+    @Override
+    public T get(long id){
+        return map.get(id);
+    }
+
+    @Override
+    public void create(T entity){
+        entity.setId(id.incrementAndGet());
+        update(entity);
+    }
+
+    @Override
+    public void update(T entity){
+       map.put(entity.getId(), entity);
+    }
+
+    @Override
+    public void delete(T entity){
+        map.remove(entity.getId());
+    }
 }
---
+++ src/main/java/com/javarush/quest/bulimov/questdelta/repository/AnswerRepository.java
@@ -0,0 +1,4 @@
+package com.javarush.quest.bulimov.questdelta.repository;
+
+public class AnswerRepository {
+}
