388423054156ff8620aa96cbd867144a6f3b6895
Danila Ryabov <danila02478@gmail.com>
2022-09-27 17:08:51 +0300

    added session info in QuestionServlet

--- src/main/java/ua/com/javarush/quest/ryabov/questdelta/controller/QuestionServlet.java
+++ src/main/java/ua/com/javarush/quest/ryabov/questdelta/controller/QuestionServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import ua.com.javarush.quest.ryabov.questdelta.entity.Answer;
 import ua.com.javarush.quest.ryabov.questdelta.entity.Question;
 import ua.com.javarush.quest.ryabov.questdelta.repository.Repository;
@@ -12,6 +13,7 @@ import ua.com.javarush.quest.ryabov.questdelta.repository.RepositoryLoader;
 import ua.com.javarush.quest.ryabov.questdelta.util.Jsp;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,8 +25,17 @@ public class QuestionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //TODO НАДО ПРОЙТИСЬ ПО СЕССИИ И УЗНАТЬ ПОСЛЕДНИЙ ВОПРОС В ТЕСТЕ
-        String question = questionRepository.get(3).get().getQuestion();
-        List<Answer> answers = questionRepository.get(3).get().getAnswers();
+        String question = null;
+        List<Answer> answers = null;
+        HttpSession session = req.getSession();
+        Optional<Question> lastQuestion = (Optional<Question>) session.getAttribute("lastQuestion");
+        if (lastQuestion == null){
+            question = questionRepository.get(3).get().getQuestion();
+            answers = questionRepository.get(3).get().getAnswers();
+        }else {
+                question = lastQuestion.get().getQuestion();
+                answers =  lastQuestion.get().getAnswers();
+        }
         req.setAttribute("question", question);
         req.setAttribute("answers", answers);
         Jsp.forward(req, resp, "question");
@@ -33,6 +44,7 @@ public class QuestionServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String nextQuestionID = req.getParameter("nextQuestionID");
+        HttpSession session = req.getSession();
         if (nextQuestionID != null){
             long id = Long.parseLong(nextQuestionID);
             Optional<Question> question = questionRepository.get(id);
@@ -40,6 +52,7 @@ public class QuestionServlet extends HttpServlet {
             List<Answer> answers = question.get().getAnswers();
             req.setAttribute("question", text);
             req.setAttribute("answers", answers);
+            session.setAttribute("lastQuestion", question);
         }
         Jsp.forward(req, resp, "question");
     }
