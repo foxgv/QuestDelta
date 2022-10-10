c61c16062c186ceb4209ab618a73a8371c74ad6e
1CC1 <volokitin_sergey@mail.ru>
2022-10-10 12:20:19 +0500

    pom fixed, finished editing questions and answers

--- src/main/java/ru/javarush/volokitin/questdelta/repository/AnswerRepository.java
+++ src/main/java/ru/javarush/volokitin/questdelta/repository/AnswerRepository.java
@@ -11,29 +11,169 @@ public class AnswerRepository extends AbstractRepository<Answer> {
     private AnswerRepository() {
         map.put(0L, Answer.with()
                 .id(0L)
-                .text("Да")
+                .text("В анонимный внутренний класс")
                 .isCorrect(true)
                 .nextQuestionId(1L)
                 .get());
 
         map.put(1L, Answer.with()
                 .id(1L)
-                .text("Нет")
+                .text("В специальный метод")
                 .isCorrect(false)
-                .nextQuestionId(null)
+                .nextQuestionId(1L)
                 .get());
 
         map.put(2L, Answer.with()
                 .id(2L)
-                .text("Вверх")
-                .isCorrect(true)
-                .nextQuestionId(null)
+                .text("В специальный объект")
+                .isCorrect(false)
+                .nextQuestionId(1L)
                 .get());
 
         map.put(3L, Answer.with()
                 .id(3L)
-                .text("Вниз")
+                .text("В специальный интерфейс")
                 .isCorrect(false)
+                .nextQuestionId(1L)
+                .get());
+
+        map.put(4L, Answer.with()
+                .id(4L)
+                .text("exit;")
+                .isCorrect(false)
+                .nextQuestionId(2L)
+                .get());
+
+        map.put(5L, Answer.with()
+                .id(5L)
+                .text("returned;")
+                .isCorrect(false)
+                .nextQuestionId(2L)
+                .get());
+
+        map.put(6L, Answer.with()
+                .id(6L)
+                .text("break;")
+                .isCorrect(true)
+                .nextQuestionId(3L)
+                .get());
+
+        map.put(7L, Answer.with()
+                .id(7L)
+                .text("continue;")
+                .isCorrect(false)
+                .nextQuestionId(2L)
+                .get());
+
+        map.put(8L, Answer.with()
+                .id(8L)
+                .text("\"Город\" к \"Карте города\"")
+                .isCorrect(false)
+                .nextQuestionId(4L)
+                .get());
+
+        map.put(9L, Answer.with()
+                .id(9L)
+                .text("\"Карта города\" к \"Городу\"")
+                .isCorrect(false)
+                .nextQuestionId(4L)
+                .get());
+
+        map.put(10L, Answer.with()
+                .id(10L)
+                .text("\"Рецепт печенья\" к \"Печенью\"")
+                .isCorrect(true)
+                .nextQuestionId(4L)
+                .get());
+
+        map.put(11L, Answer.with()
+                .id(11L)
+                .text("\"Печенье\" к \"Рецепту печенья\"")
+                .isCorrect(false)
+                .nextQuestionId(4L)
+                .get());
+
+        map.put(12L, Answer.with()
+                .id(12L)
+                .text("Int")
+                .isCorrect(false)
+                .nextQuestionId(4L)
+                .get());
+
+        map.put(13L, Answer.with()
+                .id(13L)
+                .text("Integer")
+                .isCorrect(true)
+                .nextQuestionId(4L)
+                .get());
+
+        map.put(14L, Answer.with()
+                .id(14L)
+                .text("Number")
+                .isCorrect(false)
+                .nextQuestionId(4L)
+                .get());
+
+        map.put(15L, Answer.with()
+                .id(15L)
+                .text("IntWrapper")
+                .isCorrect(false)
+                .nextQuestionId(4L)
+                .get());
+
+        map.put(16L, Answer.with()
+                .id(16L)
+                .text("Исключение автоматически обработается")
+                .isCorrect(false)
+                .nextQuestionId(5L)
+                .get());
+
+        map.put(17L, Answer.with()
+                .id(17L)
+                .text("Исключение будет автоматически логировано")
+                .isCorrect(false)
+                .nextQuestionId(5L)
+                .get());
+
+        map.put(18L, Answer.with()
+                .id(18L)
+                .text("У объекта-ресурса автоматически вызовется метод close()")
+                .isCorrect(true)
+                .nextQuestionId(5L)
+                .get());
+
+        map.put(19L, Answer.with()
+                .id(19L)
+                .text("Объект-ресурс автоматически удалится")
+                .isCorrect(false)
+                .nextQuestionId(5L)
+                .get());
+
+        map.put(20L, Answer.with()
+                .id(20L)
+                .text("Метод нельзя использовать")
+                .isCorrect(false)
+                .nextQuestionId(null)
+                .get());
+
+        map.put(21L, Answer.with()
+                .id(21L)
+                .text("Такой метод отсутствует")
+                .isCorrect(false)
+                .nextQuestionId(null)
+                .get());
+
+        map.put(22L, Answer.with()
+                .id(22L)
+                .text("Такой метод будет удален в следующей версии Java")
+                .isCorrect(false)
+                .nextQuestionId(null)
+                .get());
+
+        map.put(23L, Answer.with()
+                .id(23L)
+                .text("Метод не рекомендуется использовать")
+                .isCorrect(true)
                 .nextQuestionId(null)
                 .get());
     }
--- src/main/java/ru/javarush/volokitin/questdelta/repository/QuestionRepository.java
+++ src/main/java/ru/javarush/volokitin/questdelta/repository/QuestionRepository.java
@@ -13,18 +13,62 @@ public class QuestionRepository extends AbstractRepository<Question> {
     private QuestionRepository() {
         map.put(0L, Question.with()
                 .id(0L)
-                .text("Вопрос да или нет?")
+                .text("Лямбда-выражение преобразовывается компилятором в")
                 .answers(List.of(
                         AnswerRepository.get().get(0L),
-                        AnswerRepository.get().get(1L)))
+                        AnswerRepository.get().get(1L),
+                        AnswerRepository.get().get(2L),
+                        AnswerRepository.get().get(3L)))
                 .get());
 
         map.put(1L, Question.with()
                 .id(1L)
-                .text("Вопрос вверх или вниз?")
+                .text("Досрочно выйти из цикла можно с помощью оператора:")
                 .answers(List.of(
-                        AnswerRepository.get().get(2L),
-                        AnswerRepository.get().get(3L)))
+                        AnswerRepository.get().get(4L),
+                        AnswerRepository.get().get(5L),
+                        AnswerRepository.get().get(6L),
+                        AnswerRepository.get().get(7L)))
+                .get());
+
+        map.put(2L, Question.with()
+                .id(2L)
+                .text("«Класс» относится к «объекту» как:")
+                .answers(List.of(
+                        AnswerRepository.get().get(8L),
+                        AnswerRepository.get().get(9L),
+                        AnswerRepository.get().get(10L),
+                        AnswerRepository.get().get(11L)))
+                .get());
+
+        map.put(3L, Question.with()
+                .id(3L)
+                .text("У примитивного типа int есть его непримитивный аналог, он называется?")
+                .answers(List.of(
+                        AnswerRepository.get().get(12L),
+                        AnswerRepository.get().get(13L),
+                        AnswerRepository.get().get(14L),
+                        AnswerRepository.get().get(15L)))
+                .get());
+
+        map.put(4L, Question.with()
+                .id(4L)
+                .text("Если внутри блока try-with-resources возникнет ошибка, то:")
+                .answers(List.of(
+                        AnswerRepository.get().get(16L),
+                        AnswerRepository.get().get(17L),
+                        AnswerRepository.get().get(18L),
+                        AnswerRepository.get().get(19L)))
+                .get());
+
+        map.put(5L, Question.with()
+                .id(5L)
+                .text("IDEA отображает вам имя метода перечеркнутым горизонтальной чертой. Что это значит?\n")
+                .answers(List.of(
+                        AnswerRepository.get().get(20L),
+                        AnswerRepository.get().get(21L),
+                        AnswerRepository.get().get(22L),
+                        AnswerRepository.get().get(23L)))
                 .get());
     }
 }
